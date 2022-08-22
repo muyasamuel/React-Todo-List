a03628ca70e8e81b807b678c482c7c07b25d5b67
muyasamuel <muya.samuel@students.jkuat.ac.ke>
2022-08-22 11:54:17 +0300

    styling the input component

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from 'react';
+import classes from './Input.module.css'
 
 function input() {
   return (
-    <div>
-        <input  type='text' placeholder='Finish my react projects' />
-        <button>Submit</button>
+    <div className={classes.inputDiv}>
+        <input className={classes.inputField} type='text' placeholder='Finish my react projects' />
+        <button className={classes.actions}>Submit</button>
     </div>
   )
 }
