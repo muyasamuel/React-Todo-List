7af4e827eba9ae1e780085798eab64c609c35039
muyasamuel <muya.samuel@students.jkuat.ac.ke>
2022-08-22 13:04:00 +0300

    intriducing state to the list items

--- src/UI/Card.js
+++ src/UI/Card.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import classes from  './Card.module.css'
 
 function Card(props) {
   return (
-    <div>
+    <div className={classes.card}>
         {props.children}
     </div>
   )
--- src/components/List.js
+++ src/components/List.js
@@ -1,16 +1,45 @@
-import React from 'react';
-import classes from './List.module.css'
+import React, { useState } from 'react';
+import classes from './List.module.css';
+
+
+const todos = [
+    {
+        id: 't1',
+        name: 'Do some general house cleaning'
+    },
+    {
+        id: 't1',
+        name: 'Go for some shopping '
+    },
+    {
+        id: 't1',
+        name: 'Finish my assignments'
+    },
+    {
+        id: 't1',
+        name: 'Finish my assignments'
+    }
+
+]
 
 function List() {
+  const [ list , setList]  = useState(todos);
   return (
     <div className={classes.listDiv}>
-        <h1>hello world</h1>
-        <h1>hello world</h1>
-        <h1>hello world</h1>
-        <h1>hello world</h1>
-
-
+        {list.map((item) => {
+            return (
+                <div className={classes.listItem}>
+                    {item.name}
+                </div>
+            )
+        })}
     </div>
+       
+    
+     
+
+
+    
   )
 }
 
